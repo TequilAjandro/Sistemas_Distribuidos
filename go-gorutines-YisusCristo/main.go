4c5dea77824564a0c50592b68bd28835a11f04de
YisusCristo <alexDvlp.rr@gmail.com>
2020-12-08 14:40:03 -0600

    Go Routines

--- go-gorutines-YisusCristo/main.go
+++ go-gorutines-YisusCristo/main.go
@@ -44,7 +44,7 @@ func (p *Process) Print() {
 
 func delete(processes []Process, id int) {
 	if len(processes) == 0 {
-		fmt.Println("Lista vacia")
+		fmt.Println("No hay ningun proceso corriendo")
 	} else {
 		for i, p := range processes {
 			if p.id == id {
